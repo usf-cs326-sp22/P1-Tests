f1b7942df0ebaef76c3d5c72d68f5b856b2a1156
Matthew Malensek <matt@malensek.net>
2022-02-14 01:02:06 -0800

    Fix exit condition

--- 16-README-1.sh
+++ 16-README-1.sh
@@ -3,6 +3,6 @@ source "${TEST_LIB}/funcs.bash"
 test_start "README Check" \
     "Ensures the readme has been filled out."
 
-grep 'TODO' "${TEST_DIR}/../README.md" && test_end 0
+grep 'TODO' "${TEST_DIR}/../README.md" || test_end 0
 
 test_end 1 # If the grep above found any TODOs, the test case fails.
